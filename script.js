a665b94162bddd74eb9a48498f129ae8da7bd03e
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-09-04 21:25:30 +0300

    playRound function

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ function getComputerChoice () {
         return "paper"
     }else {return "scissors"}
 }
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection) {
+        return "Draw! No winner"
+    }else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper" ) {
+        return "You lose! Paper beats Rock"
+    }else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors" ) {
+        return "You win! Rock beats scissors"
+    }else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors" ) {
+        return "You lose! Scissors beats paper"
+    }else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock" ) {
+        return "You win! Paper beats rock"
+    }else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock" ) {
+        return "You lose! Rock beats scissors"
+    }else {return "You win! Scissors beats paper"}
+}
